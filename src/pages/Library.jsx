5687d2c9ff23c42ff2d891c0e172137651157c12
Kostas Detorakis <kostasd.777@gmail.com>
2025-03-05 23:33:43 +0200

    adding some details

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,6 +1,5 @@
 function Library() {
-    return <div className="text-center text-white text-3xl mt-10">Library Page</div>;
-  }
-  
-  export default Library;
-  
+  return <div className="text-center text-white text-3xl mt-10">Library Page</div>;
+}
+
+export default Library;
