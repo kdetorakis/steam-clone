0f7ea85c58027726df71a47ea69a262ee5971fd5
Kostas Detorakis <kostasd.777@gmail.com>
2025-03-06 11:05:08 +0200

    css changes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
